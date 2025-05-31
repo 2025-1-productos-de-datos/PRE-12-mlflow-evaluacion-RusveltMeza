739ca2f3db6913b9521d75c83289a1b0daea44d2
Rusvelt Jose Meza San Martín <rmezas@unal.edu.co>
2025-05-31 11:59:34 -0500

    2

--- homework/__main__.py
+++ homework/__main__.py
@@ -1,31 +1,6 @@
-#save model if better
+"""Entry point."""
 
-import os
-import pickle
+from .src.main import main
 
-from homework.src._internals.compare_models import compare_models
-from homework.src._internals.save_model import save_model
-
-
-def save_model_if_better(model, x_test, y_test, save_path="models/estimator.pkl"):
-    """
-    Guarda el modelo si es mejor que el modelo guardado en save_path.
-    """
-    best_model = None
-    if os.path.exists(save_path):
-        with open(save_path, "rb") as file:
-            best_model = pickle.load(file)
-
-    best_model = compare_models(model, best_model, x_test, y_test)
-    save_model(best_model, save_path)
-
-
-
-
-
-#prepare data
-
-# descarga de datos
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
+if __name__ == "__main__":
+    main()
